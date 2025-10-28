ca07c57297cf535cfa7913deae674517f3b76766
Basarath Yasin <bbox@Basaraths-MacBook-Pro.local>
2025-10-28 21:11:40 +0530

    Styling held dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,10 @@
-export default function Die({value}) {
+export default function Die({value, isHeld}) {
     
   return (
     <section>
-       <button className="dice-container">{value}</button>
+       <button style={isHeld ? { backgroundColor: "green" } : {}}>
+    {value}
+  </button>
     </section>
   )
 }
